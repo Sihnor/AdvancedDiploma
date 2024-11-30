9fff6ec825ac9f7742c91bf647464d001c7b7c81
Izmir Rexhepi <izmir.rexhepi27@hotmail.com>
2024-12-01 00:55:30 +0100

    i hate godot

--- scripts/PickupItem3D.gd
+++ scripts/PickupItem3D.gd
@@ -12,7 +12,7 @@ func _ready():
 	if main_camera == null:
 		print("Main camera not assigned!")
 		return
-	
+
 	area.input_event.connect(_on_area_3d_input_event)
 	
 	# Add this PickupItem to the "pickup_items" group
@@ -22,3 +22,7 @@ func _on_area_3d_input_event(viewport, event, event_position, normal, shape_idx)
 	if Input.is_action_pressed("uiClick"):
 		emit_signal("itemPickedUp", item_name, item_icon)  # Emit the signal locally
 		queue_free()  # Remove the pickup item from the scene after it's picked up
+
+
+func _on_button_button_down():
+	print("i got pressed")
