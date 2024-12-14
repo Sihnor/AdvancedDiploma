dcbdd7f48c7e0c76681114609bb1fdcd3844369d
Izmir Rexhepi <izmir.rexhepi27@hotmail.com>
2024-12-14 01:09:59 +0100

    book riddle done

--- BookRiddle.gd
+++ BookRiddle.gd
@@ -1,22 +1,56 @@
 extends Node3D
-
+class_name BookRiddle
 @export var solution: String 
-var input = []
+@export var shuffled: bool
+var books = []
+var inputs = []
+var isSolved = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	if shuffled:
+		shuffleBooks()
+	
+func addInput(book: Book):
+	inputs.append(book)
 
 func addBook(book: Book) -> void:
-	input.append(book)
+	books.append(book)
 
-func getBook(id: String) -> Book:
-	var result = ""
-	for book in input:
-		if book.bookID == id:
-			result = book
-	return result
+func clearInput():
+	var currentBooks = self.get_children()
+	for book in currentBooks:
+		if book is Book:
+			var t: Book = book
+			t.isSelected = false
+	inputs.clear()
+
+func swapBook(source: Book, destination: Book):
+	var sourceIndex: int = books.find(source)
+	var destinationIndex: int = books.find(destination)
+
+	# Swap the positions of the source and destination books
+	var tempPosition = source.position
+	source.position = destination.position
+	destination.position = tempPosition
+
+	# Swap the books in the array
+	var tempBook: Book = books[sourceIndex]
+	books[sourceIndex] = books[destinationIndex]
+	books[destinationIndex] = tempBook
+	pass
+
+func checkSolution():
+	var inputString :String = ""
+	for book in books:
+		var tmpBook: Book = book
+		inputString += tmpBook.bookID
+	if inputString == solution:
+		print("Solved")
+		isSolved = true
+
+func shuffleBooks():
+	# Randomize the order of books in the array
+	for i in range(len(books)):
+		var randomIndex = randi() % len(books)
+		if i != randomIndex:  # Avoid swapping an element with itself
+			swapBook(books[i], books[randomIndex])
--- scripts/Book.gd
+++ scripts/Book.gd
@@ -2,18 +2,24 @@ extends Area3D
 class_name Book
 
 @export var bookID : String
+var parent: BookRiddle
+var isSelected: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	parent = self.get_parent()
+	parent.addBook(self)
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func _on_input_event(camera, event, event_position, normal, shape_idx):
 	if Input.is_action_pressed("uiClick"):
-		print(self.name)
+		if not parent.isSolved:
+			if not isSelected:
+				if parent.inputs.size() < 2:
+					isSelected = true
+					parent.addInput(self)
+				if parent.inputs.size()>1:
+					parent.swapBook(parent.inputs[0], parent.inputs[1])
+					parent.clearInput()
+					parent.checkSolution()
 		pass # Replace with function body.
